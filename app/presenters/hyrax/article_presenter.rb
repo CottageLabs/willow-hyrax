2bdf891217a066b2e98ae5bdfd05119ad9441e47
Anusha Ranganathan <anusha3@gmail.com>
2017-08-08 12:14:55 +0100

    Views for article

--- app/presenters/hyrax/article_presenter.rb
+++ app/presenters/hyrax/article_presenter.rb
@@ -2,5 +2,7 @@
 #  `rails generate hyrax:work Article`
 module Hyrax
   class ArticlePresenter < Hyrax::WorkShowPresenter
+    delegate :doi, :publisher, :coverage, :apc, :tagged_version, :creator_nested, :date, :rights_nested,
+      :relation, :subject_nested, :project, :admin_metadata, to: :solr_document
   end
 end
